647f7daa81d8185ae5b1d6f3217162c8bc2e7b8c
Kevin Hill <kahill2014@gmail.com>
2022-06-02 01:08:40 -0500

    Make torch interactable, bat reacts to torch light

--- Assets/Scripts/BatAI.cs
+++ Assets/Scripts/BatAI.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class BatAI : MonoBehaviour
 {
     public Transform focalPoint;
+    public Torch torch;
     public int numberOfWaypoints = 10;
     public float speed = 10f;
     public float radius = 5f;
@@ -12,6 +13,8 @@ public class BatAI : MonoBehaviour
     private Vector3 target;
     private Transform _transform;
     private Vector3 _focalPosition;
+    private Vector3 _startPosition;
+    private Vector3 _tempPosition;
     private Quaternion lookOnLook;
     private Bezier myBezier;
     private float t = 0f;
@@ -19,7 +22,8 @@ public class BatAI : MonoBehaviour
     void Awake() {
         _transform = transform;
         _focalPosition = focalPoint.position;
-        _transform.position = _focalPosition;
+        _transform.position = new Vector3(_focalPosition.x, _focalPosition.y + 3f, _focalPosition.z);
+        _startPosition = _transform.position;
     }
 
     void Start() {
@@ -28,18 +32,23 @@ public class BatAI : MonoBehaviour
     }
 
     void Update() {
+        if(!torch.isLit) {
+                _transform.position = _startPosition; // TODO: Convert into flying away
+                return;
+            }
         if(Vector3.Distance(_transform.position, target) < 1) {
             IterateWaypointIndex();
             UpdateDestination();
         } else {
-            Vector3 vec = myBezier.GetPointAtTime( t );
+            Vector3 vec = myBezier.GetPointAtTime(t);
             _transform.rotation = Quaternion.Slerp(_transform.rotation, lookOnLook, speed * Time.deltaTime * 10f);
             _transform.position = vec;
 
             t += speed / 1000f;
-            if( t > 1f )
+            if(t > 1f) {
                 t = 0f;
             }
+        }
     }
 
     void OnTriggerEnter(Collider other) {
