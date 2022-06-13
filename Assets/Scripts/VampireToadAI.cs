e85126eec22c903079c985abb5972094a1b37c30
AGChow <arianagchow@gmail.com>
2022-06-13 18:07:37 -0500

    Working on Vamp toad for puzzle

--- Assets/Scripts/VampireToadAI.cs
+++ Assets/Scripts/VampireToadAI.cs
@@ -5,13 +5,19 @@ public class VampireToadAI : MonoBehaviour
     public Torch torch;
     public GameObject eyesGraphics;
     public float speed = 10f;
+    public float distanceFromPlayer = 2f;
+    public bool nearTorch;
 
 
     //animation
     // private Animator anim;
 
+    //target should be set to player in inspector
     [SerializeField] private Transform target;
     private NavMeshAgent _navMeshAgent;
+
+    private Detection detectBools;
+
 
     void Awake()
     {
@@ -19,11 +25,14 @@ public class VampireToadAI : MonoBehaviour
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _navMeshAgent.speed = speed / 3;
         _navMeshAgent.acceleration = speed / 3;
+        detectBools = GetComponentInChildren<Detection>();
     }
 
     [System.Obsolete]
     void Update()
     {
+        
+
         //behavior when torch is on
         if (torch.isLit)
         {
@@ -38,6 +47,25 @@ public class VampireToadAI : MonoBehaviour
             // anim.SetBool("Awake", true);
             eyesGraphics.SetActive(true);
         }
+
+
+
+        ///////ALT AGC trying to tie this into a way that allows any torch
+        ////behavior when torch is on
+        //if (detectBools.torchReaction == true)
+        //{
+        //    eyesGraphics.SetActive(false);
+        //    // anim.SetBool("Awake", false);
+        //    _navMeshAgent.destination = this.transform.position;
+        //}
+        ////behavior when torch is off
+        //else
+        //{
+        //    _navMeshAgent.destination = target.position;
+        //    // anim.SetBool("Awake", true);
+        //    eyesGraphics.SetActive(true);
+        //}
+
     }
 
     void OnTriggerEnter(Collider other)
